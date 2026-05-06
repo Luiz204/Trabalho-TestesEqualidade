a5b8f69b406e0834b0586a9a05e3a1905bf23bca
luiz204 <almeida_lg2004@hotmail.com>
2026-05-05 22:49:49 -0300

    fix: usa send_keys para preencher campos do checkout

--- web_tests/pages/checkout_page.py
+++ web_tests/pages/checkout_page.py
@@ -1,5 +1,6 @@
 import time
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from web_tests.pages.base_page import BasePage
 
@@ -14,28 +15,25 @@ class CheckoutPage(BasePage):
     _COMPLETE_HEADER = (By.CLASS_NAME, "complete-header")
     _PAGE_TITLE      = (By.CLASS_NAME, "title")
 
-    def _wait_for_step_one(self):
-        self.wait.until(EC.presence_of_element_located(self._FIRST_NAME))
-        time.sleep(1)
-
-    def _wait_for_step_two(self):
-        self.wait.until(EC.presence_of_element_located(self._FINISH_BTN))
-        time.sleep(1)
+    def _fill_field(self, locator, value):
+        field = self.wait.until(EC.element_to_be_clickable(locator))
+        self.driver.execute_script("arguments[0].click();", field)
+        field.clear()
+        field.send_keys(value)
+        time.sleep(0.3)
 
     def fill_personal_info(self, first_name, last_name, postal_code):
-        self._wait_for_step_one()
-        field = self.wait.until(EC.element_to_be_clickable(self._FIRST_NAME))
-        self.driver.execute_script("arguments[0].value = arguments[1];", field, first_name)
-        field = self.wait.until(EC.element_to_be_clickable(self._LAST_NAME))
-        self.driver.execute_script("arguments[0].value = arguments[1];", field, last_name)
-        field = self.wait.until(EC.element_to_be_clickable(self._POSTAL_CODE))
-        self.driver.execute_script("arguments[0].value = arguments[1];", field, postal_code)
+        self.wait.until(EC.presence_of_element_located(self._FIRST_NAME))
+        time.sleep(1)
+        self._fill_field(self._FIRST_NAME, first_name)
+        self._fill_field(self._LAST_NAME, last_name)
+        self._fill_field(self._POSTAL_CODE, postal_code)
         return self
 
     def continue_to_step_two(self):
-        element = self.wait.until(EC.presence_of_element_located(self._CONTINUE_BTN))
+        element = self.wait.until(EC.element_to_be_clickable(self._CONTINUE_BTN))
         self.driver.execute_script("arguments[0].click();", element)
-        time.sleep(3)
+        time.sleep(4)
         return self
 
     def get_error_message(self):
@@ -52,7 +50,7 @@ class CheckoutPage(BasePage):
         return self.driver.find_elements(*self._SUMMARY_ITEMS)
 
     def finish_purchase(self):
-        self._wait_for_step_two()
+        self.wait.until(EC.presence_of_element_located(self._FINISH_BTN))
         element = self.driver.find_element(*self._FINISH_BTN)
         self.driver.execute_script("arguments[0].click();", element)
         time.sleep(2)
