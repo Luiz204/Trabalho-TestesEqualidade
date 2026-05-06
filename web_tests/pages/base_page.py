6b07637da09007e0911ab99c7f500c6fd1ed949f
luiz204 <almeida_lg2004@hotmail.com>
2026-05-05 21:41:57 -0300

    fix: usa JavaScript click como fallback no headless

--- web_tests/pages/base_page.py
+++ web_tests/pages/base_page.py
@@ -2,7 +2,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 class BasePage:
-    TIMEOUT = 10
+    TIMEOUT = 20
 
     def __init__(self, driver):
         self.driver = driver
@@ -12,7 +12,11 @@ class BasePage:
         return self.wait.until(EC.presence_of_element_located(locator))
 
     def _click(self, locator):
-        self.wait.until(EC.element_to_be_clickable(locator)).click()
+        element = self.wait.until(EC.element_to_be_clickable(locator))
+        try:
+            element.click()
+        except Exception:
+            self.driver.execute_script("arguments[0].click();", element)
 
     def _type(self, locator, text):
         field = self._find(locator)
